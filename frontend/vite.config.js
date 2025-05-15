d2a935b65f94f85424dd74b0630597532b0d1097
madasamydfan <madasamyi2006@gmail.com>
2025-05-15 16:02:07 +0530

    For Frontend Deployemnt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: "./",
+    preview: {
+    host: true,  // to listen on all interfaces
+    allowedHosts: ['movie-guesser.up.railway.app'],  // add your Railway domain here
+  }
 })
