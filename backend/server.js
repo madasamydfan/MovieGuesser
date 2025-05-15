359362bad373cb91545813a07c5c533b4484a1ec
madasamydfan <madasamyi2006@gmail.com>
2025-05-15 12:20:51 +0530

    Fixed hard coded port

--- backend/server.js
+++ backend/server.js
@@ -79,6 +79,6 @@ app.post("/movieguess", async (req, res) => {
 });
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
