58de24be4c5e5cf96491f9a3da55351295d43708
madasamydfan <madasamyi2006@gmail.com>
2025-05-15 12:41:12 +0530

    Fixed hard coded port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { leaderboard } = require("./leaderboard");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { checkAnswerwithAI } = require("./checkAnswer");
-const app = express();
+const app = express();  
 app.use(cors()); // This allows all origins
 app.use(bodyParser.json());
 app.use(express.json());
@@ -78,7 +78,6 @@ app.post("/movieguess", async (req, res) => {
   }
 });
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 });
-
