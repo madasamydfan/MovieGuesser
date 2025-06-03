cc392b4773f6069f7bb11a407a355a02db1e6af6
madasamydfan <madasamyi2006@gmail.com>
2025-06-03 20:12:21 +0530

    D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import MovieGuessPage from "./components/MovieGuessPage";
-import Leaderboard from "./components/Leaderboard";
+import Leaderboard from "./components/leaderboard";
 import Instructions from "./components/Instructions";
 
 function App() {
