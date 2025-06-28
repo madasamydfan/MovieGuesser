2f026a446afa258ab92d6fcfad9a80efb27faba6
Madasamy <145176509+madasamydfan@users.noreply.github.com>
2025-06-28 11:26:56 +0530

    Update db.js

--- backend/forCreatingDataset/db.js
+++ backend/forCreatingDataset/db.js
@@ -32,17 +32,6 @@ const pool = mysql.createPool({
   }
 })();
 
-app.get('/testdb', async (req, res) => {
-  try {
-    const conn = await pool.getConnection();
-    const [rows] = await conn.query('SELECT 1 + 1 AS solution');
-    conn.release();
-    res.send('DB works: ' + rows[0].solution);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('DB Error');
-  }
-});
 
 
 // ✅ Optional: Run your init script
